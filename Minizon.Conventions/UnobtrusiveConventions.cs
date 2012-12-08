e5c2bcece4b2c870bf1523a1104e406585a89853
Sean Feldman <feldman.sean@gmail.com>
2012-12-07 21:53:11 -0700

    Added Unobtrusive Conventions for NSB

---
+++ Minizon.Conventions/UnobtrusiveConventions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NServiceBus;
+
+namespace Minizon.Conventions
+{
+    public class UnobtrusiveConventions : IWantToRunBeforeConfiguration
+    {
+        //http://www.serrate.net/2012/10/16/nservicebus-dry-with-unobtrusive-conventions/
+        public void Init()
+        {
+            Configure.Instance
+                 .DefiningCommandsAs(t => minizonNamespace(t) && t.Namespace.EndsWith("Commands"))
+                 .DefiningEventsAs(t => minizonNamespace(t) && (t.Namespace.EndsWith("Events") || t.Namespace.EndsWith("Contracts")))
+                .DefiningMessagesAs(t => minizonNamespace(t) && t.Namespace.EndsWith("Messages"));
+        }
+
+        private Func<Type, bool> minizonNamespace = (type) => type.Namespace != null && type.Namespace.StartsWith("Minizon");
+    }
+}
