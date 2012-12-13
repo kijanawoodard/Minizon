b90de0eaecb9aa038480af2a0c50cf32f333f725
Sean Feldman <feldman.sean@gmail.com>
2012-12-12 23:41:34 -0700

    - composite ui discussion Whiteboarding - areas for possible service-related UI implementations

---
+++ Minizon.Admin.Web/Areas/Pricing/PricingAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Minizon.Admin.Web.Areas.Pricing
+{
+    public class PricingAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Pricing";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Pricing_default",
+                "Pricing/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
